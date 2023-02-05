2402dacf015da56b0fca3e0d888063c8ca07691e
Bill Zhang <zchcandid@gmail.com>
2023-02-05 16:11:59 +0800

    Update utils.py
    update the token force same google client version

--- learn_gcal/cals/utils.py
+++ learn_gcal/cals/utils.py
@@ -1,6 +1,5 @@
-from os import environ
-
-from allauth.socialaccount.models import SocialToken
+import environ
+from allauth.socialaccount.models import SocialToken, SocialApp
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 
@@ -10,24 +9,34 @@ env = environ.Env()
 
 
 def get_google_credentials(token: SocialToken):
+    # please modify the name to your google app name
+    # http://127.0.0.1:8000/admin/socialaccount/socialapp/
+    try:
+        gApp = SocialApp.objects.get(name="google calender")
+    except:
+        print(""">>>please check your config on admin socialAPP,
+         default url is -> /admin/socialaccount/socialapp/""")
 
     creds = Credentials(
         token=token.token,
         refresh_token=token.token_secret,
         token_uri="https://oauth2.googleapis.com/token",
-        client_id=env("GOOGLE_CLIENT_ID", default=""),
-        client_secret=env("GOOGLE_CLIENT_SECRET", default=""),
+        client_id=gApp.client_id,
+        client_secret=gApp.secret
     )
 
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
+            r = creds.refresh(Request())
+            print(">>>>>OLD TOKEN IS --->", token.token)
+            print(">>>>>NEW TOKEN IS --->", creds.token)
             # update SocialToken
-            token.token = creds["access_token"]
+            token.token = creds.token
             token.token_secret = creds.refresh_token
             token.expires_at = creds.expiry
             token.save()
+            print(">>>>>REFRESH TOKEN IS SUCCESS")
         else:
             return NEED_REDIRECT_GOOGLE_AUTH
-
+    print(">>>>>CURRENT CREDS IS --->", creds)
     return creds
